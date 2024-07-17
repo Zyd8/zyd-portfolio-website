6de4e2e4e8e5a0d553fa8375e6f69b35d646272e
clezentine <clez@clebian.clebian.com>
2024-07-17 22:14:37 +0800

    compatability for localhost

--- zydportfolio/settings.py
+++ zydportfolio/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/zyd8/ZydPortfolioWebsite/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
